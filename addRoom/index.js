cbe29a8782f7ed69091e950a04589e478d312326
rebeckaelin <rebeckaelin@hotmail.com>
2024-09-17 11:55:00 +0200

    added error handling

--- addRoom/index.js
+++ addRoom/index.js
@@ -1,27 +1,39 @@
+const { log } = require("console")
 const {db} = require("../db")
 const {v4: uuid} = require("uuid")
 
 exports.handler = async (event) => {
     const body = JSON.parse(event.body)
-try {
-    await db.put({
-        TableName: "rooms",
-        Item: {
-            id: uuid(),
-            RoomType: body.roomType,
-            price: body.price,
-            available: body.available,
-            allowedGuests: body.allowedGuests
-        }
-    })
+    console.log(event);
     
-} catch (error) {
-    return error.message
-    
-}
 
+    if (body.roomType === undefined || body.price === undefined || body.available === undefined|| body.allowedGuests === undefined) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({message: "Please provide the following keys: roomType, price, avaiable and allowedGuests."})
+        };
+    }
+    try {
+        await db.put({
+            TableName: "rooms",
+            Item: {
+                id: uuid(),
+                RoomType: body.roomType,
+                price: body.price,
+                available: body.available,
+                allowedGuests: body.allowedGuests
+            }
+        })
+        
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: "Missing data error"})
+        }
+    }
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Room added"})
-    }
 }
+        
+    }
