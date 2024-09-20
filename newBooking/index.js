9dfa0ea4977cc58562342d8d344b877e1b8f564f
annathunberg <anna.thunberg1@gmail.com>
2024-09-20 16:14:17 +0200

    room capacity check done

--- newBooking/index.js
+++ newBooking/index.js
@@ -1,297 +1,151 @@
-const { db } = require('../db');
-const { v4: uuidv4 } = require('uuid');
+const { db } = require("../db");
+const { v4: uuidv4 } = require("uuid");
 
 exports.handler = async (event) => {
-    try {
-        const body = JSON.parse(event.body);
-        const { customerName, roomType, numberOfGuests, numberOfNights, checkIn } = body;
+  try {
+    const body = JSON.parse(event.body);
+    const { customerName, roomType, numberOfGuests, numberOfNights, checkIn } =
+      body;
 
-        // Kontrollera att alla nödvändiga fält finns
-        if (customerName === "" || undefined) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'Please correct the customerName field. Cannot be an empty string',
-                }),
-            };
-        }
-        if (roomType.length <= 0 || undefined ) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                message: 'Please correct the roomType field. Cannot be empty.',
-                    }),
-                };
-        }
-
-        if (numberOfGuests <= 0 || undefined) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                message: 'Please correct the numberOfGuests field. Cannot be less than 1.',
-                    }),
-                };
-        }
-
-        if (numberOfNights <= 0 || undefined) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                message: 'Please correct the numberOfNights field. Cannot be less than 1.',
-                    }),
-                };
-        }
-
-        if (checkIn === undefined){
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'Please correct the checkIn field, cannot be empty. Please use format 2024-10-01',
-                }),
-            };
-        }
-
-
-
-        const validateCapacity = (rooms, guests) => {
-            const totalCapacity = rooms.reduce((sum, room) => {
-            switch (room) {
-            case 'suite':
-                return sum + 3;
-            case 'double':
-                return sum + 2;
-            case 'single':
-                return sum + 1;
-                
-            default:
-                return sum
-            };
-            }, 0)                
-                return totalCapacity >= guests;
-            
-            }
-            
-        let total = validateCapacity(roomType, numberOfGuests) 
-
-        if (!total) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({message: "Sorry, youre trying to book over the capacity. Please adjust the number of guests to the number of allowed guests per room."})
-            }
-            
-        } 
-
-        const totalRooms = 20
-
-        const allBookings = await db.scan({
-            TableName: "bookings",
-
-        })
-
-        
-
-        // Om alla rumstyper är tillgängliga kan vi skapa en ny bokning
-        const newBookingParams = {
-            TableName: "bookings",
-            Item: {
-                bookingReference: uuidv4(),
-                customer: customerName,
-                typeOfRooms: roomType,
-                guests: numberOfGuests,
-                nights: numberOfNights,
-                checkInDate: checkIn,
-                booking: "room",
-                numberOfRooms: roomType.length
-            }
-        };
-        await db.put(newBookingParams);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Booking successful', Booking: newBookingParams.Item })
-        };
-            
-
-
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: error.message })
-        };
+    // Validate the required fields
+    if (!customerName || customerName === "") {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message:
+            "Please correct the customerName field. Cannot be an empty string",
+        }),
+      };
     }
+
+    if (!roomType || roomType.length <= 0) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Please correct the roomType field. Cannot be empty.",
+        }),
+      };
+    }
+
+    if (!numberOfGuests || numberOfGuests <= 0) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message:
+            "Please correct the numberOfGuests field. Cannot be less than 1.",
+        }),
+      };
+    }
+
+    if (!numberOfNights || numberOfNights <= 0) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message:
+            "Please correct the numberOfNights field. Cannot be less than 1.",
+        }),
+      };
+    }
+
+    if (!checkIn) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message:
+            "Please correct the checkIn field, cannot be empty. Please use format 2024-10-01",
+        }),
+      };
+    }
+
+    // Function to validate capacity
+    const validateCapacity = (rooms, guests) => {
+      const totalCapacity = rooms.reduce((sum, room) => {
+        switch (room) {
+          case "suite":
+            return sum + 3;
+          case "double":
+            return sum + 2;
+          case "single":
+            return sum + 1;
+          default:
+            return sum;
+        }
+      }, 0);
+      return totalCapacity >= guests;
+    };
+
+    let isValidCapacity = validateCapacity(roomType, numberOfGuests);
+
+    if (!isValidCapacity) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message:
+            "Sorry, you're trying to book over the capacity. Please adjust the number of guests.",
+        }),
+      };
+    }
+
+    const totalRooms = 20; // Example: replace with the actual total room count
+    let numberOfRooms = roomType.length;
+
+    // Scan the bookings table to count total booked rooms
+    const allBookings = await db.scan({
+      TableName: "bookings",
+    });
+
+    let totalBookedRooms = allBookings.Items.reduce((sum, booking) => {
+      return sum + booking.numberOfRooms;
+    }, 0);
+
+    console.log("Total booked rooms:", totalBookedRooms);
+
+    if (totalBookedRooms >= totalRooms) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Sorry, we're full!" }),
+      };
+    }
+
+    const availableRooms = totalRooms - totalBookedRooms;
+    if (numberOfRooms > availableRooms) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message:
+            "Sorry, we don't have enough available rooms for your request.",
+        }),
+      };
+    }
+
+    // Create a new booking
+    const newBookingParams = {
+      TableName: "bookings",
+      Item: {
+        bookingReference: uuidv4(),
+        customer: customerName,
+        typeOfRooms: roomType,
+        guests: numberOfGuests,
+        nights: numberOfNights,
+        checkInDate: checkIn,
+        booking: "room",
+        numberOfRooms: numberOfRooms,
+      },
+    };
+
+    await db.put(newBookingParams);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Booking successful",
+        Booking: newBookingParams.Item,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const availableRooms = availableRoomsParams.Items;
-// console.log(`Query Result for ${roomType}:, availableRooms`);
-
-// const totalRoomCapacity = availableRooms.reduce((sum, room) => sum + room.allowedGuests, 0);
-// totalAvailableCapacity += totalRoomCapacity;
-
-// console.log(`Available Rooms for ${roomType}:`, availableRooms);
-// console.log(`Total Capacity for ${roomType}:`, totalRoomCapacity);
-
-
-// if (availableRooms.length < requiredRooms) {
-//     return {
-//         statusCode: 400,
-//         body: JSON.stringify({
-//             message: `Too many guests for ${roomType}`
-//         }),
-//     };
-// }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     const roomData = await db.scan({
-//       TableName: 'rooms',
-//       FilterExpression:
-//         'RoomType = :roomType AND available = :available AND allowedGuests >= :numberOfGuests',
-//       ExpressionAttributeValues: {
-//         ':roomType': roomType,
-//         ':available': true,
-//         ':numberOfGuests': numberOfGuests,
-//       },
-//     });
-
-//     const availableRooms = roomData.Items;
-
-//     if (availableRooms.length === 0) {
-//       return {
-//         statusCode: 404,
-//         body: JSON.stringify({
-//           message: 'No available rooms found for the given number of guests',
-//         }),
-//       };
-//     }
-
-//     // Välj det första tillgängliga rummet
-//     const roomToBook = availableRooms[0];
-
-//     const bookingReference = uuidv4();
-
-//     // Lägg till bokningen i 'bookings' tabellen
-//     await db.put({
-//       TableName: 'bookings',
-//       Item: {
-//         bookingReference: bookingReference,
-//         CustomerId: customerName,
-//         roomId: roomToBook.id,
-//         roomType: roomType,
-//         bookingDate: new Date().toISOString(),
-//         numberOfGuests: numberOfGuests,
-//       },
-//     });
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({
-//         message: 'Room booked successfully',
-//         bookingReference: bookingReference,
-//         customerId: customerName,
-//         roomDetails: roomToBook,
-//       }),
-//     };
-//   } catch (error) {
-//     console.error('Error booking room: ', error);
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({
-//         message: 'Internal server error',
-//         error: error.message,
-//       }),
-//     };
-//   }
-// };
